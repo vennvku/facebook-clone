ee2a2f74fcce549ebf329258abed5fe87466563e
vennvku <quocvanvku@gmail.com>
2023-03-24 09:36:09 +0700

    update Newfeed

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,20 +5,32 @@ namespace App\Http\Controllers;
 use App\Http\Resources\Post as PostResource;
 use App\Http\Resources\PostCollection;
 
+use App\Friend;
+use App\Post;
+
 class PostController extends Controller
 {
     public function index()
     {
-        return new PostCollection(request()->user()->posts);
+        $friends = Friend::friendships();
+
+        if ($friends->isEmpty()) {
+            return new PostCollection(request()->user()->posts);
+        }
+
+        return new PostCollection(
+            Post::whereIn('user_id', [$friends->pluck('user_id'), $friends->pluck('friend_id')])
+                ->get()
+        );
     }
 
     public function store()
     {
         $data = request()->validate([
-            'data.attributes.body' => '',
+            'body' => '',
         ]);
 
-        $post = request()->user()->posts()->create($data['data']['attributes']);
+        $post = request()->user()->posts()->create($data);
 
         return new PostResource($post);
     }
